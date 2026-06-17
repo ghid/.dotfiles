e4889d13c50d27c83a54dc7386d86539c52d8424
Klaus-Peter Schreiner <klaus-peter.schreiner@carrier.com>
2026-06-17 10:21:29 +0200

    Restrict Mason version no longer to 1.0.0

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -443,8 +443,8 @@ require("lazy").setup({
 		"neovim/nvim-lspconfig",
 		dependencies = {
 			-- Automatically install LSPs and related tools to stdpath for Neovim
-			{ "williamboman/mason.nvim", config = true, version = "^1.0.0" }, -- NOTE: Must be loaded before dependants
-			{ "williamboman/mason-lspconfig.nvim", version = "^1.0.0" },
+			{ "mason-org/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
+			{ "mason-org/mason-lspconfig.nvim" },
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 
 			-- Useful status updates for LSP.
